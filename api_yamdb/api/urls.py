2e0109cf9f54a07f2f42b5b24a3ec5f41d984246
maxshevchdev <max.shevch@gmail.com>
2025-05-16 22:36:54 +0300

    1) Удалил urls у приложения users 2) Вернул головной url к прошлому состоянию 3) Добавил маршруты до users в приложение api

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from users.views import GetTokenView, SignUpView, UserViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('v1/', include([
+        path('auth/signup/', SignUpView.as_view(), name='signup'),
+        path('auth/token/', GetTokenView.as_view(), name='token'),
+        path('', include(router_v1.urls)),
+    ])),
+]
