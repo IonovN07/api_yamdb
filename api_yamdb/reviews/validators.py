cc8d8fd4ada10bd7ed46f3e5bbb367f6cbf6d5c4
maxshevchdev <max.shevch@gmail.com>
2025-05-28 01:34:12 +0300

    Исправлены замечания Ревьюера - изменил регулярку - убрал сортировку в выводе недопустимых символов, сделал вывод их через пробел (как показать пробел не знаю) - переместил UsernameValidatorMixin

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -10,8 +10,7 @@ def validate_username_value(value):
             f'Имя "{settings.RESERVED_NAME}" не разрешено.'
         )
 
-    allowed_char_class = settings.USERNAME_ALLOWED_CHARS
-    invalid_chars = re.sub(allowed_char_class, '', value)
+    invalid_chars = re.sub(settings.USERNAME_REGEX, '', value)
 
     if invalid_chars:
         unique_invalids = ' '.join(dict.fromkeys(invalid_chars))
